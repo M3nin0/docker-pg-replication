4e06c1e99bb96e91548f0736e549d7b6ab158176
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2021-01-19 12:02:07 -0300

    update to postgresql 12

--- slave/docker-entrypoint.sh
+++ slave/docker-entrypoint.sh
@@ -4,11 +4,12 @@ echo "*:*:*:$PG_REP_USER:$PG_REP_PASSWORD" > ~/.pgpass
 
 chmod 0600 ~/.pgpass
 
-until ping -c 1 -W 1 ${PG_MASTER_HOST:?missing environment variable. PG_MASTER_HOST must be set}
-    do
-        echo "Waiting for master to ping..."
-        sleep 1s
-done
+# until ping -c 1 -W 1 ${PG_MASTER_HOST:?missing environment variable. PG_MASTER_HOST must be set}
+#     do
+#         echo "Waiting for master to ping..."
+#         sleep 1s
+# done
+
 until pg_basebackup -h ${PG_MASTER_HOST} -D ${PGDATA} -U ${PG_REP_USER} -vP -W
     do
         echo "Waiting for master to connect..."
@@ -19,15 +20,15 @@ echo "host replication all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
 
 set -e
 
-cat > ${PGDATA}/recovery.conf <<EOF
-standby_mode = on
+cat > ${PGDATA}/postgresql.conf <<EOF
+hot_standby = on
+wal_level = replica
 primary_conninfo = 'host=$PG_MASTER_HOST port=${PG_MASTER_PORT:-5432} user=$PG_REP_USER password=$PG_REP_PASSWORD'
-trigger_file = '/tmp/touch_me_to_promote_to_me_master'
 EOF
 chown postgres. ${PGDATA} -R
 chmod 700 ${PGDATA} -R
 fi
 
-sed -i 's/wal_level = hot_standby/wal_level = replica/g' ${PGDATA}/postgresql.conf 
+# sed -i 's/wal_level = hot_standby/wal_level = replica/g' ${PGDATA}/postgresql.conf 
 
 exec "$@"
